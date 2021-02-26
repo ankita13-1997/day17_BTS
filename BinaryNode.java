37dbc5543fd8264eaa090156741c395697b6fe0a
ankita13-1997 <parhiankita@gmail.com>
2021-02-27 01:02:36 +0530

    created a pogo class for nodes of binary tree

---
+++ BinaryNode.java
@@ -0,0 +1,19 @@
+
+public class BinaryNode<E extends Comparable<E>> {
+	E key;
+	public BinaryNode leftNode;
+	public BinaryNode rightNode;
+	public BinaryNode(E key) {
+		this.key = key;
+		this.leftNode = null;
+		this.rightNode = null;
+	}
+	public int size() {
+		if(this.key == null) {
+			return 0;
+		}
+		else {
+			return (1 + this.leftNode.size() + this.rightNode.size() );
+		}
+	}
+}
